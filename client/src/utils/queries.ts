fdf15f02b96e20c9f223e45dc0d8af4051d8eb32
Mrs. Kikta <melissakikta@gmail.com>
2025-03-13 22:34:32 -0400

    mutations and queries

---
+++ client/src/utils/queries.ts
@@ -0,0 +1,72 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_GET_USERS_ALL = gql`
+  query getUsers($username: String) {
+    getUsers(username: $username) {
+      _id
+      username
+      email
+      posts {
+        _id
+        type
+        title
+        content
+        imgURL
+        likes
+        dislikes
+        comments {
+          _id
+          username
+          content
+        }
+      }
+      comments {
+        _id
+        username
+        content
+      }
+      likedPosts {
+        _id
+        type
+        title
+        content
+        imgURL
+        likes
+        dislikes
+        comments {
+          _id
+          username
+          content
+        }
+      }
+      dislikedPosts {
+        _id
+        type
+        title
+        content
+        imgURL
+        likes
+        dislikes
+        comments {
+          _id 
+          username
+          content
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_GET_USERS = gql`
+  query getUsers($username: String) {
+    getUsers(username: $username) {
+      _id
+      username
+      email
+      posts {
+        _id
+        title
+      }
+    }
+  }
+`;

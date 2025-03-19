8bcfe3085a5892047e8aadce7d331c7bb6a2ec7b
BlazeEMP <blazelitanui@gmail.com>
2025-03-19 16:36:50 -0500

    typedefs should be good to go

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -60,19 +60,21 @@ export const QUERY_GET_ALL_USERS_ALL_DATA = gql`
   	}
 `;
 
-export const QUERY_GET_USERS = gql`
-	query getUsers {
-		getUsers {
-			_id
-			username
-			email
-			posts {
-				_id
-				title
-			}
-		}
-	}
-`;
+// no user list for friends or connections right now all users and data are already available above
+
+// export const QUERY_GET_USERS = gql`
+// 	query getUsers {
+// 		getUsers {
+// 			_id
+// 			username
+// 			email
+// 			posts {
+// 				_id
+// 				title
+// 			}
+// 		}
+// 	}
+// `;
 
 export const QUERY_ME = gql`
 	query me {
@@ -126,72 +128,74 @@ export const QUERY_ME = gql`
 	}
 `;
 
-export const QUERY_GET_USER = gql`
-	query getUser($username: String!) {
-		getUser(username: $username) {
-			_id
-			username
-			email
-			posts {
-				_id
-				type
-				title
-				content
-				link
-				imgURL
-				likes
-				dislikes
-				comments {
-					_id
-					content
-					username
-					createdAt
-				}
-				createdAt
-			}    
-			comments {
-				_id
-				username
-				content
-				createdAt 
-			}
-			likedPosts {
-				_id
-				type
-				title
-				content
-				link
-				imgURL
-				likes
-				dislikes 
-				comments {
-					_id
-					username
-					content
-					createdAt
-				}
-				createdAt
-			}
-			dislikedPosts {
-				_id
-				type
-				title
-				content
-				link
-				imgURL
-				likes
-				dislikes 
-				comments {
-					_id
-					username
-					content
-					createdAt
-				}
-				createdAt
-			}
-		}
-  	}
-`;
+// no single user query used right now
+
+// export const QUERY_GET_USER = gql`
+// 	query getUser($username: String!) {
+// 		getUser(username: $username) {
+// 			_id
+// 			username
+// 			email
+// 			posts {
+// 				_id
+// 				type
+// 				title
+// 				content
+// 				link
+// 				imgURL
+// 				likes
+// 				dislikes
+// 				comments {
+// 					_id
+// 					content
+// 					username
+// 					createdAt
+// 				}
+// 				createdAt
+// 			}    
+// 			comments {
+// 				_id
+// 				username
+// 				content
+// 				createdAt 
+// 			}
+// 			likedPosts {
+// 				_id
+// 				type
+// 				title
+// 				content
+// 				link
+// 				imgURL
+// 				likes
+// 				dislikes 
+// 				comments {
+// 					_id
+// 					username
+// 					content
+// 					createdAt
+// 				}
+// 				createdAt
+// 			}
+// 			dislikedPosts {
+// 				_id
+// 				type
+// 				title
+// 				content
+// 				link
+// 				imgURL
+// 				likes
+// 				dislikes 
+// 				comments {
+// 					_id
+// 					username
+// 					content
+// 					createdAt
+// 				}
+// 				createdAt
+// 			}
+// 		}
+//   	}
+// `;
 
 export const QUERY_GET_POSTS = gql`
 	query getPosts {
@@ -216,28 +220,30 @@ export const QUERY_GET_POSTS = gql`
   	}
 `;
 
-export const QUERY_GET_POST = gql`
-	query getPost($postId: ID!) {
-		getPost(postId: $postId) {
-			_id
-			username
-			type
-			title
-			content
-			link
-			imgURL
-			likes
-			dislikes
-			comments {
-				_id
-				username
-				content
-				createdAt
-			}
-			createdAt
-		}
-	}
-`;
+// no single post query used right now
+
+// export const QUERY_GET_POST = gql`
+// 	query getPost($postId: ID!) {
+// 		getPost(postId: $postId) {
+// 			_id
+// 			username
+// 			type
+// 			title
+// 			content
+// 			link
+// 			imgURL
+// 			likes
+// 			dislikes
+// 			comments {
+// 				_id
+// 				username
+// 				content
+// 				createdAt
+// 			}
+// 			createdAt
+// 		}
+// 	}
+// `;
 
 export const QUERY_GET_COMMENTS_FOR_POST = gql`
   	query getPost($postId: ID!) {
